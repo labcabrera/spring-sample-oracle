35c6df80c995be2e5673714903a019ae445eac23
lab.cabrera@gmail.com <lab.cabrera@gmail.com>
2018-06-19 16:24:23 +0100

    Readme + minor refactor

--- src/main/java/org/lab/samples/oracle/service/claim/oracle/ClaimOracleStoredProcedure.java
+++ src/main/java/org/lab/samples/oracle/service/claim/oracle/ClaimOracleStoredProcedure.java
@@ -12,6 +12,7 @@ import org.lab.samples.oracle.model.participant.claim.OSinAccInS;
 import org.lab.samples.oracle.model.participant.claim.OSinAccOutS;
 import org.springframework.data.jdbc.support.oracle.SqlReturnStruct;
 import org.springframework.data.jdbc.support.oracle.SqlStructValue;
+import org.springframework.data.jdbc.support.oracle.StructMapper;
 import org.springframework.jdbc.core.SqlOutParameter;
 import org.springframework.jdbc.core.SqlParameter;
 import org.springframework.jdbc.object.StoredProcedure;
@@ -21,24 +22,20 @@ public class ClaimOracleStoredProcedure extends StoredProcedure {
 
 	private static final String SPROC_NAME = "MPG_K_EX_SINIESTRO_ACC_WRP.P_PROCESA_PETICION";
 
-	private StructDefinitionService definitionService;
+	private final StructMapper<OSinAccInS> requestMapper;
 
 	public ClaimOracleStoredProcedure(DataSource dataSource, StructDefinitionService definitionService) {
 		super(dataSource, SPROC_NAME);
-		this.definitionService = definitionService;
+		this.requestMapper = new CustomStructMapper<OSinAccInS>(OSinAccInS.class, definitionService);
+		SqlReturnStruct sqlReturn = new SqlReturnStruct(OSinAccOutS.class);
 		declareParameter(new SqlParameter("P_O_SIN_ACC_IN_S", Types.STRUCT, "O_SIN_ACC_IN_S"));
-		declareParameter(new SqlOutParameter("P_O_SIN_ACC_OUT_S", Types.STRUCT, "O_SIN_ACC_OUT_S",
-			new SqlReturnStruct(OSinAccOutS.class)));
+		declareParameter(new SqlOutParameter("P_O_SIN_ACC_OUT_S", Types.STRUCT, "O_SIN_ACC_OUT_S", sqlReturn));
 		compile();
 	}
 
 	public OSinAccOutS executeProc(Object... inParams) {
 		OSinAccInS request = (OSinAccInS) inParams[0];
-
-		CustomStructMapper<OSinAccInS> structMapper = new CustomStructMapper<OSinAccInS>(OSinAccInS.class,
-			definitionService);
-
-		Map in = Collections.singletonMap("P_O_SIN_ACC_IN_S", new SqlStructValue(request, structMapper));
+		Map in = Collections.singletonMap("P_O_SIN_ACC_IN_S", new SqlStructValue(request, requestMapper));
 		Map results = super.execute(in);
 		return (OSinAccOutS) results.get("P_O_SIN_ACC_OUT_S");
 	}
